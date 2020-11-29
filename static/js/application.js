4eb1e8f34b791448103a3584f445c8763b9eb16f
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-11-30 00:05:03 +0300

    Add xterm lib

--- static/js/application.js
+++ static/js/application.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var term = new Terminal();
+    term.open(document.getElementById('log'));
+    term.write('Hello from \x1B[1;3;31mGenenetwork\x1B[0m \n');
     socket.emit('gemma');
+
     socket.on('console-log', function(msg) {
         console.log("Received message: " + msg.data);
-        $('#log').append('<p>' + msg.data + '</p');
+        term.write(msg.data);
     });
 });
