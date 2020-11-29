bd31f06f5afba63d273c0930b88315a79b9caee9
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-11-30 00:42:03 +0300

    Hide mouse cursor in terminal

--- static/js/application.js
+++ static/js/application.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     var socket = io.connect('http://' + document.domain + ':' + location.port);
     var term = new Terminal();
     term.open(document.getElementById('log'));
-    term.write('Hello from \x1B[1;3;31mGenenetwork\x1B[0m \n');
+    // hide cursor:
+    term.write('\x1b[?25I]')
+    term.write('Hello from \x1B[1;3;31mGenenetwork\x1B[0m \n\r');
     socket.emit('gemma');
 
     socket.on('console-log', function(msg) {
